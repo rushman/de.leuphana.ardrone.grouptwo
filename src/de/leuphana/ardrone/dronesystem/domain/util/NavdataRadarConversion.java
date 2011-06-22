5f02df4e9f94915ef14c70a8facdabdaf5fc96da
CM <cmemail@gmx.net>
2011-06-22 15:25:57 +0200

    Updates in NavdataRadarConversion
    Change-Id: I27100710394749f0cce5f05c9e4fafa3c1bb220e

--- src/de/leuphana/ardrone/dronesystem/domain/util/NavdataRadarConversion.java
+++ src/de/leuphana/ardrone/dronesystem/domain/util/NavdataRadarConversion.java
@@ -2,31 +2,62 @@ package de.leuphana.ardrone.dronesystem.domain.util;
 
 import java.util.TimerTask;
 
+import de.leuphana.ardrone.dronesystem.communication.navdata.ICoordinateData;
 
-public class NavdataRadarConversion extends TimerTask{
-	
-	TimerTask timer = new NavdataRadarConversion();
-	
-	public void scheduleAtFixedRate(TimerTask timer, long delay, long period){
-	delay = 2000;
-	period = 500;
+/**
+ * Diese Klasse bereitet die Navdata Werte für die GUI auf.
+ * 
+ * @author cm und ls
+ * 
+ */
+
+public class NavdataRadarConversion extends TimerTask {
+
+	private float xRadarDistance;
+	private float yRadarDistance;
+	private float radarFacing;
+	private long delay = 2000;
+	private long period = 500;
+	private long conversionFactor = period;
+
+	public float getxRadarDistance() {
+		return xRadarDistance;
 	}
-		
-	
-	
+
+	public float getyRadarDistance() {
+		return yRadarDistance;
+	}
+
+	public float getRadarFacing() {
+		return radarFacing;
+	}
+
+	TimerTask timer = new NavdataRadarConversion();
+
+	/**
+	 * Aufruf des TimerTasks NavdataRadarConversion
+	 * 
+	 * @param timer
+	 *            NavdataRadarConversion Objekt
+	 * @param delay
+	 *            Startverzögerung in ms
+	 * @param period
+	 *            Aufrufintervall in ms
+	 * @throws IllegalStateException
+	 */
+	public void scheduleAtFixedRate(TimerTask timer, long delay, long period)
+			throws IllegalStateException {
+
+	}
 
 	@Override
 	public void run() {
-		// TODO Navdata abfragen
-	//x = ICoordinateData.getVx();
-		// TODO umrechnen
-		// TODO ausgeben
-		
-	}
-	
+		// TODO Datenabfrage, klappt noch nicht weil Interface noch nirgends
+		// ausprogrammiert
+		// xRadarDistance = ICoordinateData.getVx() * conversionFactor;
+		// yRadarDistance = ICoordinateData.getVy() * conversionFactor;
+		// radarFacing = Math.round(ICoordinateData.getRotate());
 
+	}
 
 }
-
-
-
